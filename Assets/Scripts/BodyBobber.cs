162059cfb689e16b8b7f036b14f5e35c32fb4da8
DuncanCorrigan <126527589+Duncan747@users.noreply.github.com>
2026-07-24 15:04:37 +1000

    Body bobbing and sinking

---
+++ Assets/Scripts/BodyBobber.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyBobber : MonoBehaviour
+{
+    public float heightThreshold;
+    public float heightLerpSpeed;
+    public float rotLerpSpeed;
+
+    public GameObject bodyPart;
+    private Vector3 restingPos;
+
+    public float heightTarg;
+    public float sinkMax;
+    public float sinkSpeed;
+    public float floatSpeed;
+    public float sinkCooldown;
+    public int sinkTimeRecoverScaler;
+
+    private float sinkTimer;
+    private float sinkTarg = 0f;
+    private bool isSinking;
+    private float recoveryMultipler = 1f;
+
+    public Transform mainCastOrigin;
+    public Transform horiCastOrigin;
+    public Transform vertCastOrigin;
+
+    private Ray mainCast;
+    private RaycastHit mainHitInfo;
+    private Ray horiCast;
+    private RaycastHit horiHitInfo;
+    private Ray vertCast;
+    private RaycastHit vertHitInfo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restingPos = bodyPart.transform.position;
+        mainCast = new Ray(mainCastOrigin.position, Vector3.down);
+        horiCast = new Ray(horiCastOrigin.position, Vector3.down);
+        vertCast = new Ray(vertCastOrigin.position, Vector3.down);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isSinking = false;
+
+            sinkTimer -= sinkCooldown / sinkTimeRecoverScaler;
+
+            if(sinkTimer < 0)
+            {
+                sinkTimer = 0;
+            }
+
+            if(recoveryMultipler < 2f)
+            {
+                recoveryMultipler += 0.2f;
+            }
+
+            StartCoroutine(FloatUp());
+        }
+        else if(sinkTimer < sinkCooldown)
+        {
+            sinkTimer += Time.deltaTime;
+        }
+        else if(sinkTimer > sinkCooldown && !isSinking)
+        {
+            isSinking = true;
+            recoveryMultipler = 1f;
+            StartCoroutine(SinkDown());
+        }    
+    }
+
+    void FixedUpdate()
+    {
+        Physics.Raycast(mainCast, out mainHitInfo);
+        Physics.Raycast(horiCast, out horiHitInfo);
+        Physics.Raycast(vertCast, out vertHitInfo);
+
+        heightTarg = heightThreshold - mainHitInfo.distance;
+
+        Vector3 mainPoint = mainHitInfo.point;
+        Vector3 horiPoint = horiHitInfo.point;
+        Vector3 vertPoint = vertHitInfo.point;
+
+        Vector3 horiNormal = mainPoint - horiPoint;
+        Vector3 vertNormal = mainPoint - vertPoint;
+
+        Quaternion horiQuat = Quaternion.AngleAxis(Vector3.SignedAngle(Vector3.right, horiNormal, Vector3.forward), Vector3.forward);
+        Quaternion vertQuat = Quaternion.AngleAxis(Vector3.SignedAngle(Vector3.forward, vertNormal, Vector3.right), Vector3.right);
+
+
+        bodyPart.transform.position = Vector3.Lerp(bodyPart.transform.position, new Vector3(bodyPart.transform.position.x, restingPos.y + (heightTarg - sinkTarg), bodyPart.transform.position.z), heightLerpSpeed);
+        bodyPart.transform.rotation = Quaternion.Lerp(bodyPart.transform.rotation, (horiQuat * vertQuat), rotLerpSpeed);
+    }
+
+    IEnumerator SinkDown()
+    {
+        float t = 0;
+        float curSinkTarg = sinkTarg;
+        while (t < sinkSpeed)
+        {
+            sinkTarg = Mathf.Lerp(curSinkTarg, sinkMax, (t / sinkSpeed));
+            t += Time.deltaTime;
+
+            if(!isSinking)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FloatUp()
+    {
+        float t = 0; 
+        float curSinkTarg = sinkTarg;
+        float newFloatSpeed = floatSpeed / recoveryMultipler;
+        while (t < newFloatSpeed)
+        {
+            sinkTarg = Mathf.Lerp(curSinkTarg, 0f, (t / newFloatSpeed));
+            t += Time.deltaTime;
+
+            if (isSinking)
+            {
+                break;
+            }
+
+            yield return null;
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                break;
+            }
+        }
+    }
+}
